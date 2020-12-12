ae408d54de0034249d119df4f80c38870e76056b
Ramtin <31255934+rradfar@users.noreply.github.com>
2020-12-12 19:28:49 +0400

    Add section on CSS selectors

--- base/js/script.js
+++ base/js/script.js
@@ -98,4 +98,37 @@ $(function () {
   //   .fadeTo(1000, 1.0)
   //   .delay(1000)
   //   .fadeOut();
+  // Select all paragraph tags
+  // var p = $("p");
+  // highlight(p);
+  // Select all elements with the class .red-box (only one in this case)
+  // var redBoxes = $(".red-box");
+  // highlight(redBoxes);
+  // Select the unique element with ID "list"
+  // var list = $("#list");
+  // highlight(list);
+  // Select all h2, p, and input tags
+  // var elements = $("h2, p, input");
+  // highlight(elements);
+  // Select only the last <li> on the page
+  // var lastItem = $("li:last");
+  // highlight(lastItem);
+  // Select all even list items on the page (every second list item)
+  // var everySecondItem = $("li:even");
+  // highlight(everySecondItem);
+  // Select all odd list items (every other list item)
+  // var everyOtherItem = $("li:odd");
+  // highlight(everyOtherItem);
+  // Select all input elements that are checked/selected
+  // Checkboxes and radio buttons don't have background-color
+  // var selected = $("input:checked");
+  // console.log(selected.length);
+  // Select all input elements of type email
+  // var emails = $("input[type=email]");
+  // highlight(emails);
 });
+
+// Helper function which highlights the given element with yellow background
+// function highlight(element) {
+//   element.css("background-color", "rgba(180, 180, 30, 0.8)");
+// }
