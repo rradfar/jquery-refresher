2dd508690e2315b59d1f4ea729edbde677522952
Ramtin <31255934+rradfar@users.noreply.github.com>
2020-12-24 09:34:40 -0800

    Add notes on event handlers

--- base/js/script.js
+++ base/js/script.js
@@ -535,6 +535,25 @@ $(function () {
         $(this).attr("src", images[i]).fadeIn();
       });
     });
+
+  // The event handlers you've used so far are only attached to those elements
+  // that exist at the time the code is executed. Thus, they won't work for
+  // dynamically attached elements:
+  $("p").click(function () {
+    $(this).slideUp();
+  });
+  $("#list").before("<p>This is a dynamically added paragraph</p>");
+
+  // To solve this, you can use delegated events. Here, you attach the handler
+  // to a parent element that will always exist on the page. When the event
+  // comes in, the parent then delegates the event to all defined children.
+  // For this, you must use the on() function, and pass in which children should
+  // handle the event. For instance, all <li> tags inside #content:
+  $("#content").on("click", "li li", function () {
+    $(this).slideUp();
+  });
+
+  $("#list ul").append("<li>Dynamically added new item!</li>");
 });
 
 // Helper function which highlights the given element with yellow background
